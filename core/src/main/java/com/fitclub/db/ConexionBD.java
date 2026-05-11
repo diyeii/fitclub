0d3421c64070479b5591dd1a16bc93dbfe71d3b6
david <diurno@B22-B4.lan.tierno.es>
2026-05-11 11:03:33 +0200

    ConexionBD

--- core/src/main/java/com/fitclub/db/ConexionBD.java
+++ core/src/main/java/com/fitclub/db/ConexionBD.java
@@ -1,4 +1,20 @@
 package com.fitclub.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionBD {
+    private static final String url = "jdbc:mysql://localhost:3306/fitclub";
+    private static final String user = "root";
+    private static final String pass = "root_password";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url,user,pass);
+        }catch (ClassNotFoundException e){
+            throw new SQLException("No se encontró el driver...", e);
+        }
+    }
 }
