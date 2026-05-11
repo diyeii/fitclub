9041c543ebc8d2f8742ca21d70b19b22e83ad73d
david <diurno@B22-B4.lan.tierno.es>
2026-05-11 12:21:13 +0200

    SocioDao

--- core/src/main/java/com/fitclub/db/SocioDao.java
+++ core/src/main/java/com/fitclub/db/SocioDao.java
@@ -10,11 +10,10 @@ public class SocioDao {
     public void insertar(Socio socio) {
         String sql = "INSERT INTO socio (nombre, apellidos, email) VALUES (?, ?, ?)";
 
-        // Fíjate que aquí usamos ConexionBd (con 'd' minúscula) como tu archivo
         try (Connection conn = ConexionBD.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            // Si en tu clase Socio los métodos están en español, usa estos:
+
             pstmt.setString(1, socio.getName());
             pstmt.setString(2, socio.getSurname());
             pstmt.setString(3, socio.getEmail());
